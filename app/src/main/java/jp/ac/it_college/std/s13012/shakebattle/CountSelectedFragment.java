0541c6100c7c2c33d12965e3614f7633e4ed38cb
Akinori Maesoko <s13012@std.it-college.ac.jp>
2014-11-12 20:14:02 +0900

    add: CountSelectedFragment

---
+++ app/src/main/java/jp/ac/it_college/std/s13012/shakebattle/CountSelectedFragment.java
@@ -0,0 +1,27 @@
+package jp.ac.it_college.std.s13012.shakebattle;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+
+public class CountSelectedFragment extends Fragment {
+
+    public CountSelectedFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_count_selected, container, false);
+    }
+}
