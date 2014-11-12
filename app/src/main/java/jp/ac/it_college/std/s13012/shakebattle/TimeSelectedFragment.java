74126b3368e64a1be9b6907195c7ee810170cede
Akinori Maesoko <s13012@std.it-college.ac.jp>
2014-11-12 22:13:38 +0900

    各ボタン押下時の処理を実装

--- app/src/main/java/jp/ac/it_college/std/s13012/shakebattle/TimeSelectedFragment.java
+++ app/src/main/java/jp/ac/it_college/std/s13012/shakebattle/TimeSelectedFragment.java
@@ -1,7 +1,7 @@
 package jp.ac.it_college.std.s13012.shakebattle;
 
-import android.app.Activity;
-import android.net.Uri;
+import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
@@ -9,6 +9,8 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class TimeSelectedFragment extends Fragment implements View.OnClickListener{
+
+    public static final String SELECTED_TIME = "selected_time";
 
     public TimeSelectedFragment() {
         // Required empty public constructor
@@ -18,11 +20,40 @@ public class TimeSelectedFragment extends Fragment implements View.OnClickListen
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_time_selected, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_time_selected, container, false);
+
+        rootView.findViewById(R.id.button_time_10sec).setOnClickListener(this);
+        rootView.findViewById(R.id.button_time_20sec).setOnClickListener(this);
+        rootView.findViewById(R.id.button_time_30sec).setOnClickListener(this);
+        rootView.findViewById(R.id.button_to_title).setOnClickListener(this);
+
+        return rootView;
+    }
+
+    private void timeSelected(int time) {
+        Intent intent = new Intent(getActivity(), TimeAttackActivity.class);
+        intent.putExtra(SELECTED_TIME, time);
+        startActivity(intent);
     }
 
     @Override
     public void onClick(View view) {
-
+        switch (view.getId()) {
+            case R.id.button_time_10sec:
+                timeSelected(10);
+                break;
+            case R.id.button_time_20sec:
+                timeSelected(20);
+                break;
+            case R.id.button_time_30sec:
+                timeSelected(30);
+                break;
+            case R.id.button_to_title:
+                getFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, new ModeFragment())
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit();
+                break;
+        }
     }
 }
