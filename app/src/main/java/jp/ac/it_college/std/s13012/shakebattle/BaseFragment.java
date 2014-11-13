d8e81e1b801a9cbe2934382831e4e5fd3240dec3
s13012 <s13012@std.it-college.ac.jp>
2014-11-13 10:47:40 +0900

    時間または回数選択後の画面遷移処理メソッド実装

--- app/src/main/java/jp/ac/it_college/std/s13012/shakebattle/BaseFragment.java
+++ app/src/main/java/jp/ac/it_college/std/s13012/shakebattle/BaseFragment.java
@@ -2,6 +2,8 @@ package jp.ac.it_college.std.s13012.shakebattle;
 
 import android.app.Fragment;
 import android.app.FragmentTransaction;
+import android.content.Context;
+import android.content.Intent;
 
 public abstract class BaseFragment extends Fragment {
 
@@ -10,6 +12,11 @@ public abstract class BaseFragment extends Fragment {
                 .replace(id, fragment)
                 .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
                 .commit();
+    }
 
+    public void selectedTimeOrCount(String name, int value, Context context, Class destination) {
+        Intent intent = new Intent(context, destination)
+                .putExtra(name, value);
+        startActivity(intent);
     }
 }
