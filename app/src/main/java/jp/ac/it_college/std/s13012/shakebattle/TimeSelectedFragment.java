4e168238d72676f88a9ce77473398f840ed2b038
s13012 <s13012@std.it-college.ac.jp>
2014-11-13 10:00:24 +0900

    不要になったImport文を削除

--- app/src/main/java/jp/ac/it_college/std/s13012/shakebattle/TimeSelectedFragment.java
+++ app/src/main/java/jp/ac/it_college/std/s13012/shakebattle/TimeSelectedFragment.java
@@ -1,9 +1,7 @@
 package jp.ac.it_college.std.s13012.shakebattle;
 
-import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
-import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
