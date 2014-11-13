770bb83cfee681e90fd3c895f62f598c08133d2c
s13012 <s13012@std.it-college.ac.jp>
2014-11-13 09:49:58 +0900

    フラグメント置換処理メソッド実装

--- app/src/main/java/jp/ac/it_college/std/s13012/shakebattle/BaseFragment.java
+++ app/src/main/java/jp/ac/it_college/std/s13012/shakebattle/BaseFragment.java
@@ -1,7 +1,15 @@
 package jp.ac.it_college.std.s13012.shakebattle;
 
 import android.app.Fragment;
+import android.app.FragmentTransaction;
 
-public class BaseFragment extends Fragment {
+public abstract class BaseFragment extends Fragment {
 
+    public void fragmentReplace(int id, Fragment fragment) {
+        getFragmentManager().beginTransaction()
+                .replace(id, fragment)
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                .commit();
+
+    }
 }
