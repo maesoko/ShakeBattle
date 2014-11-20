abfa463439abfd14c4fea8b69025856e64ac9e39
s13012 <s13012@std.it-college.ac.jp>
2014-11-20 18:52:02 +0900

    シェイク時にカウンターが変動する処理をカウントアタックに実装

--- app/src/main/java/jp/ac/it_college/std/s13012/shakebattle/CountAttackActivity.java
+++ app/src/main/java/jp/ac/it_college/std/s13012/shakebattle/CountAttackActivity.java
@@ -2,8 +2,13 @@ package jp.ac.it_college.std.s13012.shakebattle;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.SurfaceView;
 import android.view.View;
@@ -13,10 +18,20 @@ import android.view.animation.ScaleAnimation;
 import android.widget.TextView;
 
 
-public class CountAttackActivity extends Activity {
+public class CountAttackActivity extends Activity
+        implements SensorEventListener{
 
     private TimeSurfaceView mTimeSurfaceView;
     private TextView message;
+    private TextView mCountTextView;
+
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
+
+
+    private int mCounter = 0;
+    private ShakeDiscriminator shakeDiscriminator;
+    private boolean mGameIsRunning = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +43,26 @@ public class CountAttackActivity extends Activity {
 
         SurfaceView surface = (SurfaceView) findViewById(R.id.label_elapsed_time_count);
         mTimeSurfaceView = new TimeSurfaceView(surface);
+        shakeDiscriminator = new ShakeDiscriminator();
+        mCountTextView = (TextView) findViewById(R.id.current_count);
+        //センサーの準備
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         ready();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //センサーの取得
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        //センサーマネージャーへイベントリスナーを登録
+        mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
     }
 
     private void ready() {
@@ -73,15 +107,18 @@ public class CountAttackActivity extends Activity {
 
     private void gameStart() {
         mTimeSurfaceView.startMeasurement();
+        mGameIsRunning = true;
     }
 
     private void gameEnd() {
         mTimeSurfaceView.endMeasurement();
+        mGameIsRunning = false;
+        message.setText("Finish!");
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        if (event.getAction() == MotionEvent.ACTION_UP) {
+        if (event.getAction() == MotionEvent.ACTION_UP && !mGameIsRunning) {
             gameEnd();
             Intent intent = new Intent(this, ResultActivity.class);
             startActivity(intent);
@@ -90,4 +127,22 @@ public class CountAttackActivity extends Activity {
         return super.onTouchEvent(event);
     }
 
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if (sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            //振った回数が目標回数と同じになったらメソッドを抜ける
+            if (Integer.parseInt(mCountTextView.getText().toString()) ==
+                    CountSelectFragment.getGoalCount()) {
+                gameEnd();
+                return;
+            }
+            if (shakeDiscriminator.detectShake(sensorEvent) && mGameIsRunning) {
+                mCountTextView.setText(String.valueOf(mCounter++));
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+    }
 }
