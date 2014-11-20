49a642755c06622855425b358e229645133eeaa2
s13012 <s13012@std.it-college.ac.jp>
2014-11-20 19:29:50 +0900

    親クラスをShakeActivityに変更し各ゲームアクティビティの重複する処理をShakeActivityに委任

--- app/src/main/java/jp/ac/it_college/std/s13012/shakebattle/ShakeActivity.java
+++ app/src/main/java/jp/ac/it_college/std/s13012/shakebattle/ShakeActivity.java
@@ -1,5 +1,56 @@
 package jp.ac.it_college.std.s13012.shakebattle;
 
 
-public class ShakeActivity {
+import android.app.Activity;
+import android.os.Handler;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.ScaleAnimation;
+import android.widget.TextView;
+
+public abstract class ShakeActivity extends Activity{
+
+    public void ready(TextView message) {
+        message = (TextView) findViewById(R.id.message);
+        message.setVisibility(View.VISIBLE);
+        message.setText("Ready?");
+        AnimationSet animationSet = new AnimationSet(true);
+        animationSet.addAnimation(
+                new ScaleAnimation(1,2,1,2,
+                        Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f)
+        );
+
+        animationSet.setDuration(2000);
+        animationSet.setFillAfter(true);
+        message.setAnimation(animationSet);
+        final Handler handler = new Handler();
+        final TextView finalMessage = message;
+        animationSet.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                finalMessage.setText("Go!");
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        finalMessage.setText("");
+                        gameStart();
+                    }
+                }, 1000);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
+
+    public abstract void gameStart();
+    public abstract void gameEnd();
 }
--- app/src/main/java/jp/ac/it_college/std/s13012/shakebattle/TimeAttackActivity.java
+++ app/src/main/java/jp/ac/it_college/std/s13012/shakebattle/TimeAttackActivity.java
@@ -1,6 +1,5 @@
 package jp.ac.it_college.std.s13012.shakebattle;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -8,16 +7,11 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.CountDownTimer;
-import android.os.Handler;
 import android.view.MotionEvent;
-import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationSet;
-import android.view.animation.ScaleAnimation;
 import android.widget.TextView;
 
 
-public class TimeAttackActivity extends Activity
+public class TimeAttackActivity extends ShakeActivity
         implements SensorEventListener{
     private TextView timer;
     private CountDown countDown;
@@ -39,9 +33,10 @@ public class TimeAttackActivity extends Activity
         timer = (TextView) findViewById(R.id.remaining_time);
         timer.setText(String.valueOf(TimeSelectFragment.getRemainingTime() / 1000));
         countDown = new CountDown(TimeSelectFragment.getRemainingTime(),1000);
+        message = (TextView) findViewById(R.id.message);
         //センサーの準備
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        ready();
+        super.ready(message);
     }
 
     @Override
@@ -59,53 +54,16 @@ public class TimeAttackActivity extends Activity
         mSensorManager.unregisterListener(this);
     }
 
-    private void ready() {
-        message = (TextView) findViewById(R.id.message);
-        message.setVisibility(View.VISIBLE);
-        message.setText("Ready?");
-        AnimationSet animationSet = new AnimationSet(true);
-        animationSet.addAnimation(
-                new ScaleAnimation(1,2,1,2,
-                        Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f)
-        );
-
-        animationSet.setDuration(2000);
-        animationSet.setFillAfter(true);
-        message.setAnimation(animationSet);
-        final Handler handler = new Handler();
-        animationSet.setAnimationListener(new Animation.AnimationListener() {
-            @Override
-            public void onAnimationStart(Animation animation) {
-
-            }
-
-            @Override
-            public void onAnimationEnd(Animation animation) {
-                message.setText("Go!");
-                handler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        message.setText("");
-                        gameStart();
-                    }
-                }, 1000);
-            }
-
-            @Override
-            public void onAnimationRepeat(Animation animation) {
-
-            }
-        });
-    }
-
-
-    private void gameStart() {
+    @Override
+    public void gameStart() {
         mGameIsRunning = true;
         countDown.start();
     }
 
-    private void gameEnd(){
+    @Override
+    public void gameEnd() {
         mGameIsRunning = false;
+        message.setText("Finish!");
     }
 
     @Override
@@ -147,7 +105,6 @@ public class TimeAttackActivity extends Activity
         @Override
         public void onFinish() {
             timer.setText("0");
-            message.setText("Finish!");
             gameEnd();
         }
     }
