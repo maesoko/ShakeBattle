a1b723d4335f1231b89c11de7440118b6bbd6908
Akinori Maesoko <s13012@std.it-college.ac.jp>
2014-11-24 16:23:48 +0900

    サーバー側のデータ受信処理を実装

--- app/src/main/java/jp/ac/it_college/std/s13012/shakebattle/DataServerAsyncTask.java
+++ app/src/main/java/jp/ac/it_college/std/s13012/shakebattle/DataServerAsyncTask.java
@@ -1,5 +1,50 @@
 package jp.ac.it_college.std.s13012.shakebattle;
 
 
-public class DataServerAsyncTask {
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class DataServerAsyncTask extends AsyncTask<Void, Void, String> {
+
+    private TextView textView;
+
+    public DataServerAsyncTask(TextView textView) {
+        this.textView = textView;
+    }
+
+    @Override
+    protected String doInBackground(Void... voids) {
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(DataTransferService.EXTRAS_PORT_NUMBER);
+            Log.v(OpponentSearchActivity.TAG, "Server: Socket opened");
+            Socket client = serverSocket.accept();
+            Log.v(OpponentSearchActivity.TAG, "Server: connection done");
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    client.getInputStream()));
+
+            String result = br.readLine();
+            br.close();
+
+            serverSocket.close();
+            return result;
+        } catch (IOException e) {
+            Log.e(OpponentSearchActivity.TAG, e.getMessage());
+            return "error";
+        }
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        textView.setText(result);
+        Log.v(WaitOpponentActivity.TAG, "onPostExecute:" + textView.getText());
+    }
 }
